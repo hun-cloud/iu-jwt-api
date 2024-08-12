c7c798d448201ee859ea3c5b8d338b7031115a3a
JaeHunJaeHun <jhl9838@gmail.com>
2024-08-12 20:14:31 +0900

    오타 수정

--- src/main/java/com/jwt/iu/jwt/Jwts.java
+++ src/main/java/com/jwt/iu/jwt/Jwts.java
@@ -1,19 +1,16 @@
 package com.jwt.iu.jwt;
 
-import com.jwt.iu.IuJwtCreator;
-
 import static org.junit.Assert.assertNotNull;
 
 public class Jwts {
     private Header header;
     private Claim claim;
     private String key;
-    private Signature signature;
 
     public Jwts(Header header, Claim claim, String key) {
-        assertNotNull("Header can't be null",header);
+        assertNotNull("Header can't be null", header);
         assertNotNull("claim can't be null", claim);
-        assertNotNull("claim can't be null", key);
+        assertNotNull("key can't be null", key);
         this.header = header;
         this.claim = claim;
         this.key = key;
@@ -29,9 +26,5 @@ public class Jwts {
 
     public Claim getClaim() {
         return claim;
-    }
-
-    public Signature getSignature() {
-        return signature;
     }
 }
